e6e0698e8247970751d1c9cd9843661d8412a246
taznimt <taznimanwar06@gmail.com>
2024-05-14 15:59:44 +0700

    destructor

---
+++ StaticMemberFunction/staticmemberfunction4.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+	int *arr;
+	int panjang;
+public:
+	angka(int); //constructor
+	~angka(); //destructor
+	void cetakData();
+	void isiData();
+};
+//defenisi member function
+
+angka::angka(int i) { //constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+angka::~angka() { //destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan " << endl;
+}
+void angka::cetakData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+
+	}
+}
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = ";
+		cin >> arr[i];
+	}
+	cout << endl;
+}
+int main()
+{
+	angka belajarcpp(3); //constructor dipanggil
+	angka* ptrBelajarcpp = new angka(5); //constructor dipanggil
+	delete ptrBelajarcpp; //destructor dipanggil
+	return 0;
+}//destructor dipanggil
