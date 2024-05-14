c45f2e382a23ef244be561627c68f309d7a1cd84
taznimt <taznimanwar06@gmail.com>
2024-05-14 14:51:24 +0700

    menampilkan printall mahasiswa

--- StaticMemberFunction/StaticMemberFunction.cpp
+++ StaticMemberFunction/StaticMemberFunction.cpp
@@ -21,6 +21,12 @@ void mahasiswa::setID()
 {
 	id = ++nim;
 }
+void mahasiswa::printAll()
+{
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout <<  endl;
+}
 int main()
 {
    
